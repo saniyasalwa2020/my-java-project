434b139c7f4b17d561b1d5eb065a49945127188e
saniyasalwa2020 <122582082+saniyasalwa2020@users.noreply.github.com>
2025-05-01 11:31:15 +0530

    Task 3: Hello World demo application using SLF4J UPDATED

--- Main.java
+++ Main.java
@@ -7,6 +7,6 @@ public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
  
     public static void main(String[] args) {
-        logger.info("🟢 Task 3: Hello, World! The demo service is running successfully.");
+        logger.info("🟢 Task 3: Hello, World! Demo app is running successfully ✅");
     }
 }
