5371c36c376b0b3c9dac323c3dc383f20afb89f1
saniyasalwa2020 <122582082+saniyasalwa2020@users.noreply.github.com>
2025-05-01 10:08:11 +0530

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,17 +1,15 @@
 package com.example;
  
-public class Main {
-
-    public static void main(String[] args) {
-
-        System.out.println("Hello from Maven!");
-
-
-     System.out.println("🚀 Maven Build Success!");
-     
-
-    }
-
-}
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
  
+public class Main {
+    // Create a logger instance
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+ 
+    public static void main(String[] args) {
+        logger.info("🚀 Maven Build Success with Logging!");
+        logger.debug("This is a DEBUG log example.");
+        logger.error("This is an ERROR log example (just for demo).");
+    }
+}
