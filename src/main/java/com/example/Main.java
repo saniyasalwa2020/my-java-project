2875be1d8a027b8f9596969fe69820d18f27b33b
saniyasalwa2020 <122582082+saniyasalwa2020@users.noreply.github.com>
2025-05-01 10:18:08 +0530

    Updated Main.java to use SLF4J logger instead of System.out

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,15 +1,17 @@
-package com.example;
- 
 import org.slf4j.Logger;
+
 import org.slf4j.LoggerFactory;
  
 public class Main {
-    // Create a logger instance
+
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
  
     public static void main(String[] args) {
-        logger.info("🚀 Maven Build Success with Logging!");
-        logger.debug("This is a DEBUG log example.");
-        logger.error("This is an ERROR log example (just for demo).");
+
+        logger.info("Hello from Maven with Logging!");
+
     }
+
 }
+
+ 
